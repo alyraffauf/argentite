66971110914383169016f5e309b23cbeb78f1d67
Aly Raffauf <aly@aly.codes>
2026-01-31 22:40:12 -0500

    add vicinae

--- build/03-third-party-packages.sh
+++ build/03-third-party-packages.sh
@@ -47,6 +47,8 @@ echo "::endgroup::"
 
 echo "::group:: Install COPR Packages"
 
+copr_install_isolated "scottames/vicinae" "vicinae"
+
 copr_install_isolated scottames/ghostty "ghostty"
 
 copr_install_isolated "ublue-os/packages" \
